736e536208de010958bff24f1f4d33d57cf03cac
Jose Antonio Bermudo Olea <bermudo.olea.jose.antonio@alumnat.copernic.cat>
2023-05-26 12:35:55 +0200

    HallOfFame  merge resolve conflics

--- src/main/java/logica/HallOfFameController.java
+++ src/main/java/logica/HallOfFameController.java
@@ -1,10 +1,10 @@
-<<<<<<< HEAD
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package logica;
 
+import common.Utils;
 import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
@@ -13,6 +13,7 @@ import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.ListView;
+import javafx.scene.control.TextArea;
 import logica.utils.LoadFXML;
 import presentacion.PresentationLayer;
 
@@ -34,70 +35,19 @@ public class HallOfFameController extends PresentationLayer implements Initializ
     @FXML
     private Button btnMenuPrincipal;
     
+    @FXML
+    private TextArea lvLogger;
+    
     private LoadFXML loadFXML = new LoadFXML();
 
     @FXML
     void onActionMenuPrincipal(ActionEvent event) throws IOException {
-        loadFXML.changeScreen("logica/main.fxml", btnMenuPrincipal);
+        loadFXML.changeScreen("main", btnMenuPrincipal);
     }
 
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        Manager.getInstance().addController(this);
-    }
-
-    @Override
-    public void close() {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
-    }
-
-}
-=======
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-package logica;
-
-import common.Utils;
-import java.io.IOException;
-import java.net.URL;
-import java.util.ResourceBundle;
-import javafx.event.ActionEvent;
-import javafx.fxml.FXML;
-import javafx.fxml.Initializable;
-import javafx.scene.control.Button;
-import javafx.scene.control.ListView;
-import javafx.scene.control.TextArea;
-
-/**
- *
- * @author ivan
- */
-public class HallOfFameController implements Initializable {
-
-    @FXML
-    private ResourceBundle resources;
-
-    @FXML
-    private URL location;
-
-    @FXML
-    private ListView<?> lvScores;
-
-    @FXML
-    private Button btnMenuPrincipal;
-    
-    @FXML
-    private TextArea lvLogger;
-
-    @FXML
-    void onActionMenuPrincipal(ActionEvent event) throws IOException {
-        App.setRoot("main");
-    }
-
-    @Override
-    public void initialize(URL url, ResourceBundle rb) {
+         Manager.getInstance().addController(this);
         try {
             //cargar puntuaciones de la BBDD
             lvLogger.appendText("Las puntuaciones están actualizadas a " + Utils.getCurrentDateTime() + "\n");
@@ -106,5 +56,9 @@ public class HallOfFameController implements Initializable {
         }
     }
 
+    @Override
+    public void close() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
 }
->>>>>>> ad8e9a1d7f7c90b0ecf9e3a55dd3ca6bd3a80fa5
